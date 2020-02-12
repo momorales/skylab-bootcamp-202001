a3c3d93e88fa8bc65d83e11720bf2fa11bbf5131
kike4815 <e.pedros.sanchez@gmail.com>
2020-02-12 20:17:14 +0100

    add sass login

--- staff/groups/hearthstone-group/hearthstone/hearthstone-app/components/login.jsx
+++ staff/groups/hearthstone-group/hearthstone/hearthstone-app/components/login.jsx
@@ -1,5 +1,6 @@
 function Login({ onSubmit, onToRegister, error}) {
-    return <form className = 'login'
+    return <div className="container-login">
+    <form className = 'login'
         onSubmit={event => {
             event.preventDefault()
 
@@ -9,17 +10,17 @@ function Login({ onSubmit, onToRegister, error}) {
             onSubmit(username, password)
         }}>
 
-
-        <h2>Login</h2>
+        <h2><img src='../hearthstone-template/logo.png' className= 'logo' /></h2>
 
         <input type="text" name="username" placeholder="Username" ></input>
         <input type="text" name="password" placeholder="Password" ></input>
         {error && <Feedback level='error' message={error} />}
-        <button>Login</button>
+        <button className = "login-button">Login</button>
         <a href=""
             onClick = {event =>{
                 event.preventDefault()
                 onToRegister()
             }}>Register</a>
     </form>
+    </div>
 }
