b093221c34fa9bf2cbc7e29312844587025b8c53
Alex Park <alexparkvinyas@gmail.com>
2020-02-12 10:15:55 +0100

    first star rating approach

--- staff/groups/hearthstone-group/hearthstone/hearthstone-app/components/card.jsx
+++ staff/groups/hearthstone-group/hearthstone/hearthstone-app/components/card.jsx
@@ -8,7 +8,15 @@ function Card({ cardInfo: { id, name, image } , onClick, onWL, onDeck}) {
        { !image && <img src="https://legaldbol.com/wp-content/uploads/2019/03/48-Free-Printable-Card-Template-Hearthstone-in-Photoshop-by-Card-Template-Hearthstone.jpg"
             onClick={() => onClick(id)} />}
 
-        <span>10/10 (500)</span>
+        <div>
+            <span><i className="fas fa-star checked"></i></span>
+            <span><i className="fas fa-star checked"></i></span>
+            <span><i className="fas fa-star checked"></i></span>
+            <span><i className="fas fa-star"></i></span>
+            <span><i className="fas fa-star"></i></span>
+            <p>7.5/10 (2)</p>
+            
+        </div>
 
         <button onClick={() => onWL(id)}>Add to wishlist</button>
         <button onClick={() => onDeck(id)}>Add to deck</button>
