3b7278c8db69a030b9b757fda2d7935af2e2de83
momorales <monimoralesr77@gmail.com>
2020-02-19 19:06:07 +0100

    finish CHL

--- staff/monica-morales/web-server/index.js
+++ staff/monica-morales/web-server/index.js
@@ -1,58 +1,23 @@
-// const http = require ('http')
-// const fs = require ('fs')
-// const log = require ('./logger')
-
-// const {argv: [, , port = 8080]} = process
-
-// const requestListener = (req, res) => {
-
-//   const path = req.url
-
-//   const rs = fs.createReadStream(`.${path === '/' ? '/index.html' : path}`)
-  
-//     if(path !== 'favicon.ico'){
-//       rs.on('data', body => {
-//       res.end(body);
-
-//       log.info(`request from ${req.connection.remoteAddress}`)
-//       console.log(req)
-//     })
-//       rs.on('error', error => {
-//       log.error(error)
-//       res.writeHead(404)
-//       res.end('NOT FOUND')
-
-//     })
-//   }else{
-//       log.error(error)
-//       res.writeHead(404)
-//       res.end('NOT FOUND')
-//   }
-
-//   req.on('error', error =>{
-//       log.error(error)
-//       res.writeHead(404)
-//       res.end('NOT FOUND')
-//   })
-// }
-
-// log.info('starting server')
-
-// const server = http.createServer(requestListener);
-// server.listen(port);
-
-
 const express = require ('express')
 const path = require('path')
+const logger = require('./utils/logger')
+const loggerMidWare = require('./utils/logger-mid-ware')
 
 const {argv: [, , port = 8080]} = process
 
+logger.level = logger.DEBUG
+logger.path = path.join(__dirname, 'server.log')
+
 const app = express()
 
-app.use(express.static('public'));
+app.use(loggerMidWare));
 
-app.get('/', (req, res) => {
-  res.send(req)
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.listen(port)
+
+process.on('SIGINT', () => {
+    logger.warn(`server abruptly stopped`)
+
+    process.exit(0)
 })
-
-app.listen(port);
