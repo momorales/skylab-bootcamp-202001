3ff95ea49d55a3ca9db79e684be00d69752ce4b3
momorales <monimoralesr77@gmail.com>
2020-02-19 17:51:55 +0100

    update http server

--- staff/monica-morales/web-server/index.js
+++ staff/monica-morales/web-server/index.js
@@ -1,44 +1,58 @@
-const http = require ('http')
-const fs = require ('fs')
-const log = require ('./logger')
+// const http = require ('http')
+// const fs = require ('fs')
+// const log = require ('./logger')
+
+// const {argv: [, , port = 8080]} = process
+
+// const requestListener = (req, res) => {
+
+//   const path = req.url
+
+//   const rs = fs.createReadStream(`.${path === '/' ? '/index.html' : path}`)
+  
+//     if(path !== 'favicon.ico'){
+//       rs.on('data', body => {
+//       res.end(body);
+
+//       log.info(`request from ${req.connection.remoteAddress}`)
+//       console.log(req)
+//     })
+//       rs.on('error', error => {
+//       log.error(error)
+//       res.writeHead(404)
+//       res.end('NOT FOUND')
+
+//     })
+//   }else{
+//       log.error(error)
+//       res.writeHead(404)
+//       res.end('NOT FOUND')
+//   }
+
+//   req.on('error', error =>{
+//       log.error(error)
+//       res.writeHead(404)
+//       res.end('NOT FOUND')
+//   })
+// }
+
+// log.info('starting server')
+
+// const server = http.createServer(requestListener);
+// server.listen(port);
+
+
+const express = require ('express')
+const path = require('path')
 
 const {argv: [, , port = 8080]} = process
 
-const requestListener = (req, res) => {
+const app = express()
 
-  const path = req.url
+app.use(express.static('public'));
 
-  const rs = fs.createReadStream(`.${path === '/' ? '/index.html' : path}`)
-  
-    if(path !== 'favicon.ico'){
-      rs.on('data', body => {
-      res.end(body);
+app.get('/', (req, res) => {
+  res.send(req)
+})
 
-      log.info(`request from ${req.connection.remoteAddress}`)
-      console.log(req)
-    })
-      rs.on('error', error => {
-      log.error(error)
-      res.writeHead(404)
-      res.end('NOT FOUND')
-
-    })
-  }else{
-      log.error(error)
-      res.writeHead(404)
-      res.end('NOT FOUND')
-  }
-
-  req.on('error', error =>{
-      log.error(error)
-      res.writeHead(404)
-      res.end('NOT FOUND')
-  })
-}
-
-log.info('starting server')
-
-const server = http.createServer(requestListener);
-server.listen(port);
-
-
+app.listen(port);
