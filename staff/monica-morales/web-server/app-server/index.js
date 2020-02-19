8cb03638363ec2bf390f739231df0f86b0ecb776
momorales <monimoralesr77@gmail.com>
2020-02-19 20:23:03 +0100

    create structure files

---
+++ staff/monica-morales/web-server/app-server/index.js
@@ -0,0 +1,28 @@
+const express = require ('express')
+const path = require('path')
+const logger = require('./utils/logger')
+const loggerMidWare = require('./utils/logger-mid-ware')
+const authenticate = require('./logic/authenticate')
+const authenticate = require('./logic/register')
+const data = require('./utils/data')
+
+
+
+const {argv: [, , port = 8080]} = process
+
+logger.level = logger.DEBUG
+logger.path = path.join(__dirname, 'server.log')
+
+const app = express()
+
+app.use(loggerMidWare));
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.listen(port)
+
+process.on('SIGINT', () => {
+    logger.warn(`server abruptly stopped`)
+
+    process.exit(0)
+})
