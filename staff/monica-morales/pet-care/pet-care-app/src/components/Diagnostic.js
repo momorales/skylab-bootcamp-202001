779c056bb08098eca67922fdbc624586a1f08375
momorales <monimoralesr77@gmail.com>
2020-03-31 11:52:14 +0200

    diagnostics

--- staff/monica-morales/pet-care/pet-care-app/src/components/Diagnostic.js
+++ staff/monica-morales/pet-care/pet-care-app/src/components/Diagnostic.js
@@ -5,7 +5,7 @@ Moment.globalFormat = 'D MMM YYYY'
 
 
 export default ({diagnostic}) => {
-    const {name, test, description, lab, dateCreate, petId, id} = diagnostic
+    const {name, test, description, lab, dateCreate, idPet, id} = diagnostic
 
     return <>
 
@@ -21,3 +21,34 @@ export default ({diagnostic}) => {
     </>
 }
 
+// export default function ({pet, onGoToDiagnostic, error}) {
+//     const { numberChip, name, dateOfBirth, specie, sex,sterilized, weight, race, typeRace, fur, user,createdDate, diagnostic, _id} = pet
+
+//     const [petId , setPetId] = useState()
+
+//     useEffect(() => {
+//        setPetId(_id)
+//     }, [])    
+
+//     function handleDiagnostic(event) {
+//         event.preventDefault()
+
+//         onGoToDiagnostic(petId)
+//     }
+
+//     return <>
+//         <section className = "container-detail">
+//             <div className="container-detail__detailPet">
+//                 <p>My pet {name}</p>  
+//             </div>
+//             <ul>
+//                 <li><span>Pet Name: {name}</span></li>
+//                 <li><span> Chip number: {numberChip}</span></li>
+//                 <li><span> Specie: {specie}</span></li>
+//                 <li><span> Sterilized: {sterilized}</span></li>
+//                 <li><span> Weight: {weight} Kg</span></li>
+//             </ul>
+
+//             <button onClick= {handleDiagnostic} className= "container-detail__buttonDiagnostics">Diagnostics</button>
+
+//         </section>
